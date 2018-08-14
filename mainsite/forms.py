ba473bf40bca549ccd0a48127d7f8401a0f24c47
Leelb <884610374@qq.com>
2018-08-14 18:27:19 +0800

    Picture model from

--- mainsite/forms.py
+++ mainsite/forms.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from django.forms import ModelForm, Textarea
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from .models import User
+from .models import User, PictureEntry
 
 
 class SiteUser(AuthenticationForm):
@@ -17,3 +18,14 @@ class SiteUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = User
         fields = ['username', 'password']
+
+
+class PublishImgForm(ModelForm):
+
+    class Meta:
+        model = PictureEntry
+        fields = ['img_url', 'description']
+        widgets = {
+            'img_url': Textarea(attrs={'cols': 50, 'rows': 10}),
+            'description': Textarea(attrs={'cols': 50, 'rows': 20}),
+        }
--- mainsite/models.py
+++ mainsite/models.py
@@ -84,11 +84,12 @@ class UserAuthBackend:
 
 class PictureEntry(models.Model):
     uploader = models.ForeignKey(User, on_delete=models.CASCADE)
-    img_file = models.ImageField(upload_to='')
-    description = models.CharField(max_length=500)
+    # img_file = models.ImageField(upload_to='')
+    img_url = models.URLField(max_length=300)
+    description = models.CharField(max_length=500, null=True, blank=True)
     upload_time = models.DateTimeField(auto_now_add=True)
-    approval = models.IntegerField()
-    negative = models.IntegerField()
+    approval = models.IntegerField(default=0)
+    negative = models.IntegerField(default=0)
 
     def __str__(self):
         pass
