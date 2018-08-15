04d767df8bff1446c774621cefa4b4e4ca66ac82
Leelb <884610374@qq.com>
2018-08-16 00:31:09 +0800

    views

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from django.urls import path
+from . import views
 
 app_name = 'miansite'
 urlpatterns = [
-    path('user/signup/'),
-    path('user/login/'),
+    path('', '', name='index'),
+    path('user/register/', views.register_view, name='register'),
+    path('user/login/', views.login_view, name='login'),
+    path('user/logout/', views.logout_view, name='logout')
 ]
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout
 from .forms import *
@@ -6,18 +7,32 @@ from .forms import *
 # Create your views here.
 
 
+def register_view(request):
+    pass
+
+
 def login_view(request):
-    if request.user.is_authenticated:
-        login(request, request.user)
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+            login(request, request.user)
+            return redirect(reverse('mainsite:index'))
+    elif request.method == 'GET':
+        return render(request, 'mainsite/login_page.html', {
+             'form': PublishImgForm,
+        },)
 
 
 def logout_view(request):
     logout(request)
+    return redirect(reverse('mainsite:index'))
 
 
-@login_required()
+@login_required(login_url=reverse('mainsite:login'))
 def get_publication_img(request):
-    # type
+    # 之后可以添加类型type，处理多种上传
     new_pub_form = PublishImgForm(request.POST)
     if new_pub_form.is_valid():
-        new_pub_form.save()
+        request.user.pictureentry_set.create(
+            img_url=new_pub_form.cleaned_data['img_url'],
+            description=new_pub_form['description'],
+        )
