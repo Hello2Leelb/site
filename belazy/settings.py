35fd162d15ca17e8d2d804bb8e491c97b787ddd7
Leelb <884610374@qq.com>
2018-08-21 18:30:32 +0800

    ',' in settings

--- belazy/settings.py
+++ belazy/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'mainsite.apps.MainsiteConfig'
+    'mainsite.apps.MainsiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
