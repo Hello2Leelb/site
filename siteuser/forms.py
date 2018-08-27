9430870af11439503f39849de3add77aa069a45c
Leelb <884610374@qq.com>
2018-08-28 00:39:48 +0800

    confirm login form, register form;

--- siteuser/forms.py
+++ siteuser/forms.py
@@ -7,10 +7,17 @@ from siteuser.models import User as SiteUser
 
 # 参考自带表单设计，用于自定义网站用户model
 class AuthSiteUserForm(AuthenticationForm):
+    """
+    登录验证表单，包括 username, password
+    """
     pass
 
 
 class SiteUserCreationForm(UserCreationForm):
+    """
+    注册账户，基于model User，包括 username, email, password1, password2
+    其中，email非必需
+    """
 
     class Meta(UserCreationForm.Meta):
         model = SiteUser
